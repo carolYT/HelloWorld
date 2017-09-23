b5a4e40284c51c9c4d34625b7661147f816a30b5
YZcarol123@163.com <YZcarol123.163.com>
2017-09-23 21:17:02 +0800

    append GPL

---
+++ app/src/main/java/com/example/carol_yt/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.carol_yt.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
